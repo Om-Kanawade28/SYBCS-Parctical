f5ceaf7ddd26322e577e87a4457c554c71b71179
Om-Kanawade28 <154841806+Om-Kanawade28@users.noreply.github.com>
2024-07-19 16:03:58 +0530

    pattern

---
+++ Day 2/19_pattern.py
@@ -0,0 +1,9 @@
+
+def pattern(n):
+    for i in range(0,n):
+        for j in range(0,n-i-1):
+            print(" ",end="")
+        for j in range(0,i+1):
+            print("* ",end="")
+        print("\n")
+pattern(5)
