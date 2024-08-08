25e0423ba73596d9f534b90bc9324d46c848f721
Om-Kanawade28 <154841806+Om-Kanawade28@users.noreply.github.com>
2024-08-08 13:52:11 +0530

    class

---
+++ Day 4/01_class.py
@@ -0,0 +1,46 @@
+class emp:
+    emp_id = 0
+    emp_name = ''
+    emp_city = ''
+    emp_salary = 0
+print("Employee details:-\n")
+print("****************************************************")
+emp1 = emp()
+emp1.emp_id = 1
+emp1.emp_name = 'om'
+emp1.emp_city = 'pune'
+emp1.emp_salary = 20000000
+print(f"Employee Id = {emp1.emp_id}\n name = {emp1.emp_name}\n city = {emp1.emp_city}\n salary = {emp1.emp_salary}$")
+
+print("****************************************************")
+emp2 = emp()
+emp2.emp_id = 2
+emp2.emp_name = 'rohit'
+emp2.emp_city = 'pune'
+emp2.emp_salary = 20000
+print(f"Employee Id = {emp2.emp_id}\n name = {emp2.emp_name}\n city = {emp2.emp_city}\n salary = {emp2.emp_salary}$")
+print("****************************************************")
+emp3 = emp()
+emp3.emp_id = 3
+emp3.emp_name = 'onkar'
+emp3.emp_city = 'pune'
+emp3.emp_salary = 48922
+print(f"Employee Id = {emp3.emp_id}\n name = {emp3.emp_name}\n city = {emp3.emp_city}\n salary = {emp3.emp_salary}$")
+print("****************************************************")
+emp4 = emp()
+emp4.emp_id = 4
+emp4.emp_name = 'raj'
+emp4.emp_city = 'nahik'
+emp4.emp_salary = 100000
+print(f"Employee Id = {emp4.emp_id}\n name = {emp4.emp_name}\n city = {emp4.emp_city}\n salary = {emp4.emp_salary}$")
+print("****************************************************")
+emp5 = emp()
+emp5.emp_id = 5
+emp5.emp_name = 'saisha'
+emp5.emp_city = 'nimgoan'
+emp5.emp_salary = 10000
+print(f"Employee Id = {emp5.emp_id}\n name = {emp5.emp_name}\n city = {emp5.emp_city}\n salary = {emp5.emp_salary}$")
+print("****************************************************")
+
+
+    
