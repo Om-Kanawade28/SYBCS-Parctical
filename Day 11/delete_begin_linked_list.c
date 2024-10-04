4e362c041b3d2835d41f0fccd95bc686596bd4cf
Om-Kanawade28 <154841806+Om-Kanawade28@users.noreply.github.com>
2024-10-04 13:18:41 +0530

    s

---
+++ Day 11/delete_begin_linked_list.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+    int data;
+    struct node *next;
+};
+int main(){
+    struct node *head,*newnode,*tail;
+    head = NULL;
+    int choice = 1;
+    while(choice)
+
+    return 0;
+}
